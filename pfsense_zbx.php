0eb8fa3edb9a6b666f3cd5b8212e1ca1d84fe033
Ely Deckers <e.deckers@gmail.com>
2022-02-20 23:05:23 +0100

    Fix mapping issue in openvpn_client

--- pfsense_zbx.php
+++ pfsense_zbx.php
@@ -410,7 +410,7 @@ class Discoveries
     {
         self::print_json(array_map(fn($client) => [
             "{#CLIENT}" => $client["vpnid"],
-            "{#NAME}", self::sanitize_name($client["name"]),
+            "{#NAME}" => self::sanitize_name($client["name"]),
         ], PfEnv::openvpn_get_active_clients()));
     }
 
